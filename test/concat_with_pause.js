477cd15bbc3c095d18e2066a513ea535de337a68
Christopher Mühl <christopher@padarom.io>
2019-02-10 20:47:02 +0100

    Remove WAV header from test files

--- test/concat_with_pause.js
+++ test/concat_with_pause.js
@@ -4,20 +4,22 @@ const fs = require('fs')
 
 // Audio files are all in 16-bit signed PCM format
 const audioFiles = [
-    './audio/one.wav',
-    './audio/two.wav',
-    './audio/three.wav',
-    './audio/four.wav',
-    './audio/five.wav',
+    './audio/one.pcm',
+    './audio/two.pcm',
+    './audio/three.pcm',
+    './audio/four.pcm',
+    './audio/five.pcm',
+    './audio/whitenoise.pcm',
 ]
 
 let speaker = new Speaker()
 let mixer = new Mixer()
-mixer.pipe(speaker)
-
+mixer.start().pipe(speaker)
+/*
 audioFiles.forEach((file, n) => {
     let stream = fs.createReadStream(file)
     let input = mixer.input({ delay: n * 2000 })
 
     stream.pipe(input)
 })
+*/
